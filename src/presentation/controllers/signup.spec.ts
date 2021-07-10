6fbc0faffc9113efe728403890d11b96ca8e38a5
jose.paulo <jose.paulo@irancho.com.br>
2021-07-10 14:24:44 -0300

    feat: ensure SignUpController return an error if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -4,7 +4,6 @@ describe('SgnUp Controller', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
-        name: 'any_name',
         email: 'any_email@mail.com',
         password: 'any_password',
         passwordConfirmation: 'any_password'
@@ -13,5 +12,19 @@ describe('SgnUp Controller', () => {
    const httpRequese = sut.handle(httpRequest)
    expect(httpRequese.statusCode).toBe(400)  
    expect(httpRequese.body).toEqual(new Error('Missign param: name'))  
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+   const httpRequese = sut.handle(httpRequest)
+   expect(httpRequese.statusCode).toBe(400)  
+   expect(httpRequese.body).toEqual(new Error('Missign param: email'))  
   })
 })
