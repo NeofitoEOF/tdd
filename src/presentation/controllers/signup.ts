604bd2a2084f8683f360f040771454b8284c4049
jose.paulo <jose.paulo@irancho.com.br>
2021-07-10 15:09:42 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,11 +3,11 @@ import { MissingParamError } from '../erros/missing-param-error'
 import { badRequest  } from '../helpers/http-helper'
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse{
-    if(!httpRequest.body.name){
-      return badRequest( new MissingParamError('name'))
-    }
-    if(!httpRequest.body.email){
-      return badRequest (new MissingParamError('email'))
+    const requiredFields = ['name', 'email']
+    for(let fied of requiredFields){
+      if(!httpRequest.body[fied]){
+        return badRequest (new MissingParamError(fied))
+      }
     }
   }
 }
